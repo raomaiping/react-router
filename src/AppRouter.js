6ba5f4a23f533deb695014f58db94d61fd02eeed
2582395486@qq.com <2582395486@qq.com>
2020-03-13 00:18:25 +0800

    精确匹配和页面分离

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
+import Index from './pages/Index'
+import List from './pages/List'
 
-function Index(){
-    return <h2>前端小菜鸟吖</h2>
-}
 
-function List(){
-    return <h2>List-page</h2>
-}
 
 function AppRouter(){
     return (
@@ -17,7 +13,7 @@ function AppRouter(){
                 <li><Link to="/list/">列表</Link></li>
             </ul>
             <Route path="/" exact component={Index} />
-            <Route path="/list" exact component={List} />
+            <Route path="/list"  component={List} />
         </Router>
     )
 }
---
+++ src/pages/Index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <h2>前端小菜鸟吖</h2>
+         );
+    }
+}
+ 
+export default Index;
