46bff715a722937d23ef128cc3039eb592dce275
2582395486@qq.com <2582395486@qq.com>
2020-03-13 10:36:51 +0800

    react-router动态传值2

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 class Index extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            list:[
+                {cid:123,title:"前端小菜鸟的个人博客-1"},
+                {cid:456,title:"前端小菜鸟的个人博客-2"},
+                {cid:789,title:"前端小菜鸟的个人博客-3"}
+            ]
+         }
     }
     render() { 
         return ( 
-            <h2>前端小菜鸟吖</h2>
+            <div>
+                <h2>前端小菜鸟吖</h2>
+                <ul>
+                    {
+                        this.state.list.map((item,index)=>{
+                            return (
+                                <li key={index+item}>
+                                    <Link to={'/list/'+item.cid}>
+                                        {item.title}
+                                    </Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
          );
     }
 }
